3ddad2412bab4bea8be50dd3d78eecf214eb57b9
Kay <kayvins@gmail.com>
2022-03-31 03:47:29 +0200

    using fs module

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-console.log('Loading a module')
-const mod = require('./mod')
-console.log('Mod:', mod)
-console.log('Done')
+// console.log('Loading a module')
+// const mod = require('./mod')
+// console.log('Mod:', mod)
+// console.log('Done')
+
+let fs = require("fs");
+fs.readFile("./ELEN4010.txt", "utf8", (err, data) => {
+// utf8 is the encoding of the file
+if (err) throw err; //callbacks usually get errors in this format
+//err will either contain the error or null
+console.log(data); //if there weren't errors, send the data to the console
+})
