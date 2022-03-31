29bb9d808cdf5c9f19779e927d31454d0f2f74a8
Kay <kayvins@gmail.com>
2022-03-31 05:59:28 +0200

    small edit

--- Lab2_Web/index.js
+++ Lab2_Web/index.js
@@ -17,4 +17,4 @@ const mainRouter = require('./mainRoutes')
     app.use(mainRouter)
     
     app.listen(3000)
-    console.log('Express server running on port 3000')
+    console.log('Express server running on port(s) 3000')
