0761c43d9d6d7d6ad41fe26b785e1c5515512137
Kay <kayvins@gmail.com>
2022-03-31 04:36:46 +0200

    Exercise 11

--- main.js
+++ main.js
@@ -4,6 +4,14 @@
 // console.log('Done')
 
 let fs = require("fs");
+var dolor = require('dolor');
+
+
+fs.appendFile("./ELEN4010.txt", dolor.sentence(3), function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+  });
+
 fs.readFile("./ELEN4010.txt", "utf8", (err, data) => {
 // utf8 is the encoding of the file
 if (err) throw err; //callbacks usually get errors in this format
